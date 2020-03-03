214ccd0b900380e4a28f7da1af1f5bc81eedb0d9
Shitao Fan <43820194+ShitaoFan@users.noreply.github.com>
2020-03-03 13:28:32 +0000

    camera RayCast

---
+++ OculusQuestVREyeGaze/Assets/Scripts/Arrays.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public GameObject[] objects;
+
+    void Start ()
+    {
+        objects = GameObject.FindGameObjectsWithTag("Object");
+        
+        for(int i = 0; i < 1; i++)
+        {
+            Debug.Log("Object Number "+i+" is named "+objects[i].name);
+        }
+        for(int i = 0; i < 2; i++)
+        {
+            Debug.Log("Object Number "+i+" is named "+objects[i].name);
+        }
+        for(int i = 0; i < 3; i++)
+        {
+            Debug.Log("Object Number "+i+" is named "+objects[i].name);
+        }
+    }
+}
---
+++ OculusQuestVREyeGaze/Assets/Scripts/RayCastScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Ray ray; 
+    RaycastHit hit;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            Debug.Log(hit.collider.name);
+        }
+
+        Debug.DrawRay(transform.position,transform.forward*1000.0f,Color.red);
+    }
+}
